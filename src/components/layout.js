4ff4d0306f1951730c59a515b21a712adf8f8e5a
niawjunior <pasupol@dome.cloud>
2020-03-01 00:00:00 +0700

    add rel=preconnect

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,7 +24,7 @@ class Template extends React.Component {
         <Helmet>
           <link
             href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap"
-            rel="stylesheet"
+            rel="preconnect"
           />
         </Helmet>
         <GlobalStyle />
