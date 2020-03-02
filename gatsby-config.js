fb5902d9c8eb46fd66ffdc7d8bc39224946348ae
niawjunior <pasupol@dome.cloud>
2020-03-02 15:27:58 +0700

    migrate pure css to tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -107,5 +107,17 @@ module.exports = {
         modules: ['gatsby-starter-morning-dew'],
       },
     },
+    // Add after these plugins if used
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        // develop: true, // Enable while using `gatsby develop`
+        tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        ignore: ['prismjs/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      },
+    },
   ],
 }
--- src/components/Flag/Flag.js
+++ src/components/Flag/Flag.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
-
 import ukFlag from './uk.png'
 import thFlag from './th.png'
-
+import tw from 'tailwind.macro'
 const FlagImage = styled.img`
-  padding-right: 0.5rem;
-  padding-bottom: 0.2rem;
-  width: 30px;
+  ${tw`pr-2 pb-1 w-10`}
 `
 
 const Flag = ({ language }) => {
   const img = language === 'th' ? thFlag : ukFlag
   const alt = language === 'th' ? 'thai post' : 'english post'
 
-  return <FlagImage src={img} alt={alt} className="flag" />
+  return <FlagImage src={img} alt={alt} />
 }
 
 export default Flag
